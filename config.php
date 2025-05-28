64534cccc17c3bc413e931266850c85d3ad392e7
TofuWho <jcayabyab655@gmial.com>
2025-05-29 00:54:27 +0800

    Rename Classes, new design

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 // config.php
 // Define a constant for the base path to the includes directory
 define('BASE_PATH', __DIR__ . '/includes/');
-define('BASE_URL', '/ThesisLibrary/ThesisLibrary/'); // Adjusted to include the additional directory
+define('BASE_URL', '/ThesisLibrary/'); // Adjusted to include the additional directory
 ?>
