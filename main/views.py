254e2865ef47ffa2d7290dc29827182be57d8fb4
TofuWho <jcayabyab655@gmial.com>
2025-07-08 02:21:38 +0800

    auth fixes (django 4.2)

--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'main/landing.html')
+
+def about_page(request):
+    return render(request, 'main/about.html')
