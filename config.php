8cc3e3021f4389332390f99439f71377f41f5154
TofuWho <jcayabyab655@gmail.com>
2025-08-06 19:14:20 +0800

    cleaning unsused files

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// config.php
-// Define a constant for the base path to the includes directory
-define('BASE_PATH', __DIR__ . '/includes/');
-define('BASE_URL', '/ThesisLibrary/'); // Adjusted to include the additional directory
-?>
