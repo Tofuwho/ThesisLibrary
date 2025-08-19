59b3f3c15772bca32bf29c835bf06ed32240aa8b
TofuWho <jcayabyab655@gmail.com>
2025-08-19 14:37:18 +0800

    Model Changes, Filtering, Get request optimization, media storage to handle view and download of Thesis,, and minor fixes

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.landing_page, name='landing'),
@@ -8,4 +10,9 @@ urlpatterns = [
     path('categories/', views.categories_page, name='categories'),
     path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
     path('thesis/<int:pk>/', views.thesis_detail, name='thesis_detail'),
+    path('thesis/<int:pk>/view/', views.view_thesis_file, name='thesis_view_file'),
+    path('thesis/<int:pk>/download/', views.download_thesis_file, name='thesis_download_file'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
