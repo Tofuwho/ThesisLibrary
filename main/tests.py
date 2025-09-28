55d8e70f3f12238c4a2f69a271cf6a21938f20ff
Ahdonis <migueldennis04@gmail.com>
2025-09-29 06:39:41 +0800

    Unit-Test-Login

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LoginTestCase(TestCase):
+    def setUp(self):
+        # Create a user for login
+        self.user = User.objects.create_user(
+            username="Student01",
+            email="Student01@gmail.com",
+            password="Student01"
+        )
+
+    def test_login_with_valid_credentials(self):
+        response = self.client.post(reverse("login"), {
+            "username": "Student01",
+            "password": "Student01"
+        })
+        # Check redirect (successful login)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("landing"))
