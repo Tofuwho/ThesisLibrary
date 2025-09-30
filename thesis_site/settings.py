23b4a4b0bc0a0219fb3efd345a82f2d86e78005b
Ahdonis <migueldennis04@gmail.com>
2025-09-30 19:41:34 +0800

    Update main 12.0

--- thesis_site/settings.py
+++ thesis_site/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('MYSQL_DATABASE', 'thesis_library'),
         'USER': os.environ.get('MYSQL_USER', 'root'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'root'),
-        'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+        'HOST': os.environ.get('MYSQL_HOST', 'mysql'),
         'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
 }
