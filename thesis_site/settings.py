efeb3f05aead8ecccbdf462bcec76ff1ff13844f
Ahdonis <migueldennis04@gmail.com>
2025-09-30 18:41:09 +0800

    Update main 12.0

--- thesis_site/settings.py
+++ thesis_site/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'thesis_library',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'mysql',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
