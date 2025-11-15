140524e772078c517f3e005324d56dd9ea466bec
Lemon <lemonkarlmaverick@gmail.com>
2025-11-16 02:10:25 +0800

    Created_at student_list fix.

---
+++ main/middleware/timezone_middleware.py
@@ -0,0 +1,9 @@
+from django.utils import timezone
+
+class UserTimezoneMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        timezone.activate("Asia/Manila")
+        return self.get_response(request)
